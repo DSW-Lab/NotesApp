b999a969d89500ce047a087e842b80139ebd457c
Nacho009 <72605872+Nacho009@users.noreply.github.com>
2023-03-06 00:47:58 +0100

    a

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_05_194153) do
-  create_table "clients", force: :cascade do |t|
-    t.string "firstname"
-    t.string "lastname"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-  
   create_table "users", force: :cascade do |t|
     t.string "firstname"
     t.string "lastname"
