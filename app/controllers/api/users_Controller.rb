f1ec3ac53b0abb2799783809bab006cebabeb6c1
Nacho009 <72605872+Nacho009@users.noreply.github.com>
2023-03-11 16:55:38 +0100

    avance controller

--- app/controllers/api/users_Controller.rb
+++ app/controllers/api/users_Controller.rb
@@ -1,4 +1,19 @@
-class UsersController < ApplicationController
+module Api
+  
+  class UsersController < ApplicationController
+
+
+    def index
+      users = User.order('created_at')
+
+      render json: {
+
+        status: 'YES!',
+        message: 'Users loaded',
+        data: users
+      }, status: :ok
+    end
+
     def new
         @user = User.new(user_params)
     end
@@ -7,7 +22,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            redirect_to users_path
+          redirect_to users_path
         else
             # render "PONER PAGINA QUE DEBE IR"
         end
@@ -18,9 +33,10 @@ class UsersController < ApplicationController
         if @user.destroy
           redirect_to users_path
         end
-      end
+    end
     
     def user_params
         params.require(:user).permit(:email, :password, :phone)
-      end
     end
+  end
+end
