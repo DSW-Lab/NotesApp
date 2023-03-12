e8d7dd019d425fcbd9f245504184eeba44f94c2f
Esther Camacho <Esther.Camacho@alu.uclm.es>
2023-03-12 18:02:59 +0100

    Creamos el modelo Note y su controlador

---
+++ app/models/Note.rb
@@ -0,0 +1,5 @@
+class Note < ApplicationRecord
+
+    # Asociación entre la nota y el usuario que la crea
+    belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_05_194153) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_12_165714) do
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id"
+    t.boolean "is_shared"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "firstname"
