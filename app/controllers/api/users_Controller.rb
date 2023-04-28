a80031fa10de457c526309c6929f162932ca6c15
Esther Camacho <Esther.Camacho@alu.uclm.es>
2023-04-28 16:53:54 +0200

    Actualizar user_controller

--- app/controllers/api/users_Controller.rb
+++ app/controllers/api/users_Controller.rb
@@ -82,7 +82,7 @@
       private
   
       def user_params
-        params.require(:user).permit(:email, :firstname, :lastname, :password, :confirmpassword)
+        params.require(:user).permit(:name, :email, :password)
       end
   
   
