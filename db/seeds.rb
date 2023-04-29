eece5d9593caf5bcfd7e24ed42a3d5872a59e599
Sandra Ciudad <72567156+sandraciudad1@users.noreply.github.com>
2023-04-29 19:12:13 +0200

    Add files via upload

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,35 @@
 User.create({
 
-    email: "nacho@gmail.com",
-    firstname: "Nacho",
-    lastname: "Ramos",
-    password: "1234567890",
-    confirmpassword: ""
+    email: "prueba123@gmail.com",
+    firstname: "Prueba",
+    lastname: "Rodriguez",
+    password: "12345090",
+    #confirmpassword: ""
 
+})
+
+User.create({
+
+    email: "hola@gmail.com",
+    firstname: "hola",
+    lastname: "adios",
+    password: "123",
+    #confirmpassword: ""
+
+})
+
+user = User.create(email: "pruebahola@gmail.com", firstname: "hola", lastname: "adios", password: "123")
+
+Note.create({
+    title: "Mi primera nota",
+    content: "Este es el contenido de mi primera nota",
+    user_id: "",
+    is_shared: false
+})
+
+Note.create({
+    title: "Hola que tal",
+    content: "Soy el usuario 1",
+    user_id: "1",
+    is_shared: false
 })
